5f8c0a8b2f9375df99af8e9b916ba91500ffe6a4
andriusmw <andriusmw@yahoo.es>
2022-05-28 09:14:29 +0200

    levantando el servidor porque la nación no hay quien la levante

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+
+/********************** REQUIREMENTS ************ */
+const chalk = require("chalk");
 const express = require("express");
+const res = require("express/lib/response");
 
 const app = express();
 
+/*********************** RUTAS ************** */
+
+app.get("/search", (req,res) => {
+    console.log(req.query);
+    res.end();
+});
+
+/********************** LEVANTANDO EL SERVIDOR ********** */
 app.listen(3000, () => {
-    console.log("Se ha iniciado el servidor en el puesto 3000")
+    console.log(chalk.green("Se ha iniciado el servidor en el puesto 3000"))
 });
